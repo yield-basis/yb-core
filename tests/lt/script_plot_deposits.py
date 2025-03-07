860a846282507cc72ca5fe5768f8052cc2879ebf
Michael Egorov <michwill@gmail.com>
2025-03-07 18:15:32 +0100

    Add script to plot LT deposit value vs imbalance

---
+++ tests/lt/script_plot_deposits.py
@@ -0,0 +1,36 @@
+import boa
+import pylab
+import numpy as np
+
+
+# It is not a test but a plotting script
+def test_deposit_discount(cryptopool, seed_cryptopool, yb_allocated, yb_lt, yb_amm, collateral_token, stablecoin, admin):
+    # 100k USD + 100k worth BTC in pool
+    collateral_token._mint_for_testing(admin, 10 * 10**18)
+    stablecoin._mint_for_testing(admin, 10**6 * 10**18)
+    deposit_amount = 10**18
+    p = 100_000
+    relative_changes = np.linspace(0, 0.5, 100)
+    relative_changes = list(-relative_changes[1:][::-1]) + list(relative_changes)
+    misbalances = []
+    values = []
+
+    with boa.env.prank(admin):
+        yb_lt.deposit(deposit_amount, p * deposit_amount, 0)
+        collateral0, debt0, _ = yb_amm.get_state()
+
+        for r in list(relative_changes):
+            with boa.env.anchor():
+                # Prepare state - disbalance
+                if r > 0:
+                    amount = int(r * debt0)
+                    yb_amm.exchange(0, 1, amount, 0)
+                elif r < 0:
+                    amount = int(-r * collateral0)
+                    yb_amm.exchange(1, 0, amount, 0)
+                misbalances.append(r)
+                value = yb_lt.preview_deposit(deposit_amount, p * deposit_amount)
+                values.append(value)
+
+    pylab.plot(misbalances, values)
+    pylab.show()
