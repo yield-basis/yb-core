6b39a74efe6e7205955907f80e2ae679aff81bfd
Michael Egorov <michwill@gmail.com>
2025-06-01 19:50:16 +0200

    Framework for stateful test of gauges

--- tests/dao/test_gauge.py
+++ tests/dao/test_gauge.py
@@ -1,5 +1,8 @@
 import pytest
 import boa
+from hypothesis import settings
+from hypothesis import strategies as st
+from hypothesis.stateful import RuleBasedStateMachine, run_state_machine_as_test, rule, invariant
 from .conftest import N_POOLS
 
 
@@ -12,13 +15,18 @@ def dummy_factory(gc, admin):
 
 
 @pytest.fixture(scope="session")
-def gauges(mock_lp, gc, dummy_factory, admin):
+def gauges(mock_lp, gc, dummy_factory, admin, accounts):
     gauge_deployer = boa.load_partial('contracts/dao/LiquidityGauge.vy')
     with boa.env.prank(dummy_factory.address):
         gauges = [gauge_deployer.deploy(mock_lp.address) for i in range(N_POOLS)]
     with boa.env.prank(admin):
         for gauge in gauges:
             gc.add_gauge(gauge.address)
+    for user in accounts:
+        with boa.env.prank(user):
+            for g in gauges:
+                mock_lp.approve(g.address, 2**256 - 1)
+                mock_lp._mint_for_testing(user, 10**40)
     return gauges
 
 
@@ -34,5 +42,49 @@ def vote_for_gauges(gauges, yb, ve_yb, gc, accounts, admin):
         gc.vote_for_gauge_weights(gauges, VOTES)
 
 
-def test_test(gauges, gc, accounts, vote_for_gauges):
-    pass
+class StatefulG(RuleBasedStateMachine):
+    user_id = st.integers(min_value=0, max_value=9)
+    gauge_id = st.integers(min_value=0, max_value=N_POOLS - 1)
+    token_amount = st.integers(min_value=0, max_value=10**25)
+    dt = st.integers(min_value=0, max_value=30 * 86400)
+
+    @rule(uid=user_id, assets=token_amount, gid=gauge_id)
+    def deposit(self, uid, assets, gid):
+        user = self.accounts[uid]
+        with boa.env.prank(user):
+            self.gauges[gid].deposit(assets, user)
+
+    @rule(uid=user_id, shares=token_amount, gid=gauge_id)
+    def withdraw(self, uid, shares, gid):
+        user = self.accounts[uid]
+        with boa.env.prank(user):
+            if shares <= self.gauges[gid].balanceOf(user):
+                self.gauges[gid].redeem(shares, user, user)
+
+    def transfer(self):
+        pass
+
+    def claim(self):
+        pass
+
+    @invariant()
+    def check_adjustment(self):
+        for g in self.gauges:
+            assert g.get_adjustment() <= 10**18
+
+    def check_mint_sum(self):
+        pass
+
+    def check_mint_split(self):
+        pass
+
+    @rule(dt=dt)
+    def time_travel(self, dt):
+        boa.env.time_travel(dt)
+
+
+def test_gauges(mock_lp, gauges, gc, accounts, vote_for_gauges):
+    StatefulG.TestCase.settings = settings(max_examples=100, stateful_step_count=100)
+    for k, v in locals().items():
+        setattr(StatefulG, k, v)
+    run_state_machine_as_test(StatefulG)
