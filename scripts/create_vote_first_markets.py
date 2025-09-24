ae4d0321188805cc9c5b4cd87473a6954bd34d2f
Michael Egorov <michwill@gmail.com>
2025-09-24 16:52:30 +0200

    Vote for first markets

---
+++ scripts/create_vote_first_markets.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+import boa
+import os
+import json
+import requests
+
+from eth_account import account
+from collections import namedtuple
+from getpass import getpass
+from networks import NETWORK
+from networks import ETHERSCAN_API_KEY
+from networks import PINATA_TOKEN
+from time import sleep
+
+from boa.explorer import Etherscan
+from boa.verifiers import verify as boa_verify
+
+
+FORK = True
+VOTING_PLUGIN = "0x2be6670DE1cCEC715bDBBa2e3A6C1A05E496ec78"
+USER = "0xa39E4d6bb25A8E55552D6D9ab1f5f8889DDdC80d"
+
+EXTRA_TIMEOUT = 10
+
+
+Proposal = namedtuple("Proposal", ["metadata", "actions", "allowFailureMap", "startDate", "endDate", "voteOption",
+                                   "tryEarlyExecution"])
+Action = namedtuple("Action", ["to", "value", "data"])
+
+
+def pin_to_ipfs(content: dict):
+    url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    headers = {
+        "Authorization": f"Bearer {PINATA_TOKEN}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "pinataContent": content,
+        "pinataMetadata": {"name": "pinnie.json"},
+        "pinataOptions": {"cidVersion": 1},
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    assert 200 <= response.status_code < 400
+
+    return 'ipfs://' + response.json()["IpfsHash"]
+
+
+def verify(*args, **kw):
+    while True:
+        try:
+            sleep(EXTRA_TIMEOUT)
+            boa_verify(*args, **kw)
+            break
+        except ValueError as e:
+            print(e)
+            if "Already Verified" in str(e):
+                return
+
+
+def account_load(fname):
+    path = os.path.expanduser(os.path.join('~', '.brownie', 'accounts', fname + '.json'))
+    with open(path, 'r') as f:
+        pkey = account.decode_keyfile_json(json.load(f), getpass())
+        return account.Account.from_key(pkey)
+
+
+if __name__ == '__main__':
+    if FORK:
+        boa.fork(NETWORK)
+        boa.env.eoa = USER
+    else:
+        boa.set_network_env(NETWORK)
+        USER = account_load('yb-deployer')
+        boa.env.add_account(USER)
+        etherscan = Etherscan(api_key=ETHERSCAN_API_KEY)
+
+    voting = boa.load_abi(os.path.dirname(__file__) + '/TokenVoting.abi.json', name="AragonVoting").at(VOTING_PLUGIN)
+    factory = boa.load_abi(os.path.dirname(__file__) + '/Factory.abi.json', name="YB").at("0x370a449FeBb9411c95bf897021377fe0B7D100c0")
+
+    amm_interface = boa.load_partial('contracts/AMM.vy')
+    yb_amm_impl = amm_interface.deploy_as_blueprint()
+    if not FORK:
+        yb_amm_impl.ctor_calldata = b""
+        verify(yb_amm_impl, etherscan, wait=True)
+    lt_interface = boa.load_partial('contracts/LT.vy')
+    yb_lt_impl = lt_interface.deploy_as_blueprint()
+    if not FORK:
+        yb_lt_impl.ctor_calldata = b""
+        verify(yb_lt_impl, etherscan, wait=True)
+    gauge_impl = boa.load_partial('contracts/dao/LiquidityGauge.vy').deploy_as_blueprint()
+    if not FORK:
+        gauge_impl.ctor_calldata = b""
+        verify(gauge_impl, etherscan, wait=True)
+
+    proposal_id = voting.createProposal(*Proposal(
+        metadata=pin_to_ipfs({
+            'title': 'Create first Yield Basis markerts',
+            'summary': 'Create first YB markets with 2M crvUSD for each: with WBTC, cbBTC, tBTC collaterals',
+            'resources': []}).encode(),
+        actions=[
+            Action(to=factory.address, value=0,
+                   data=factory.add_market.prepare_calldata(
+                        "0xD9FF8396554A0d18B2CFbeC53e1979b7ecCe8373",
+                        int(0.0092 * 1e18),
+                        int(0.035 * 1e18 / (86400 * 365)),
+                        2 * 10**6 * 10**18
+                       )
+                   ),
+            Action(to=factory.address, value=0,
+                   data=factory.add_market.prepare_calldata(
+                        "0x83f24023d15d835a213df24fd309c47dAb5BEb32",
+                        int(0.0092 * 1e18),
+                        int(0.035 * 1e18 / (86400 * 365)),
+                        2 * 10**6 * 10**18
+                       )
+                   ),
+            Action(to=factory.address, value=0,
+                   data=factory.add_market.prepare_calldata(
+                        "0xf1F435B05D255a5dBdE37333C0f61DA6F69c6127",
+                        int(0.0092 * 1e18),
+                        int(0.035 * 1e18 / (86400 * 365)),
+                        2 * 10**6 * 10**18
+                       )
+                   ),
+        ],
+        allowFailureMap=0,
+        startDate=0,
+        endDate=0,
+        voteOption=0,
+        tryEarlyExecution=True
+    ))
+    print(proposal_id)
