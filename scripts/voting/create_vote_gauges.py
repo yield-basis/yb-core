d2bfbd514c93c30fe3191ad9217a1eae7b75007f
Michael Egorov <michwill@gmail.com>
2025-10-08 20:03:50 +0200

    Vote to add gauges - script

---
+++ scripts/voting/create_vote_gauges.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import boa
+import os
+import json
+import requests
+
+from eth_account import account
+from collections import namedtuple
+from getpass import getpass
+from networks import NETWORK
+from networks import ETHERSCAN_API_KEY
+from networks import PINATA_TOKEN
+from time import sleep
+
+from boa.explorer import Etherscan
+from boa.verifiers import verify as boa_verify
+
+
+FORK = True
+VOTING_PLUGIN = "0x2be6670DE1cCEC715bDBBa2e3A6C1A05E496ec78"
+USER = "0xa39E4d6bb25A8E55552D6D9ab1f5f8889DDdC80d"
+
+EXTRA_TIMEOUT = 10
+
+
+Proposal = namedtuple("Proposal", ["metadata", "actions", "allowFailureMap", "startDate", "endDate", "voteOption",
+                                   "tryEarlyExecution"])
+Action = namedtuple("Action", ["to", "value", "data"])
+
+
+def pin_to_ipfs(content: dict):
+    url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    headers = {
+        "Authorization": f"Bearer {PINATA_TOKEN}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "pinataContent": content,
+        "pinataMetadata": {"name": "pinnie.json"},
+        "pinataOptions": {"cidVersion": 1},
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    assert 200 <= response.status_code < 400
+
+    return 'ipfs://' + response.json()["IpfsHash"]
+
+
+def verify(*args, **kw):
+    while True:
+        try:
+            sleep(EXTRA_TIMEOUT)
+            boa_verify(*args, **kw)
+            break
+        except ValueError as e:
+            print(e)
+            if "Already Verified" in str(e):
+                return
+
+
+def account_load(fname):
+    path = os.path.expanduser(os.path.join('~', '.brownie', 'accounts', fname + '.json'))
+    with open(path, 'r') as f:
+        pkey = account.decode_keyfile_json(json.load(f), getpass())
+        return account.Account.from_key(pkey)
+
+
+if __name__ == '__main__':
+    if FORK:
+        boa.fork(NETWORK)
+        boa.env.eoa = USER
+    else:
+        boa.set_network_env(NETWORK)
+        USER = account_load('yb-deployer')
+        boa.env.add_account(USER)
+        etherscan = Etherscan(api_key=ETHERSCAN_API_KEY)
+
+    voting = boa.load_abi(os.path.dirname(__file__) + '/TokenVoting.abi.json', name="AragonVoting").at(VOTING_PLUGIN)
+    factory = boa.load_abi(os.path.dirname(__file__) + '/Factory.abi.json', name="Factory").at("0x370a449FeBb9411c95bf897021377fe0B7D100c0")
+    gc = boa.load_abi(os.path.dirname(__file__) + '/GC.abi.json', name="GC").at(factory.gauge_controller())
+
+    gauges = [factory.markets(i).staker for i in range(3)]
+
+    proposal_id = voting.createProposal(*Proposal(
+        metadata=pin_to_ipfs({
+            'title': 'Add gauges for WBTC, cbBTC and tBTC pools to GaugeController',
+            'summary': 'This is the last step necessary before turning YB emissions on [https://forum.yieldbasis.com/t/add-gauges-for-all-the-existing-pools-in-order-to-turn-yb-emissions-on/12]',
+            'resources': []}).encode(),
+        actions=[
+            Action(to=gc.address, value=0, data=gc.add_gauge.prepare_calldata(g))
+            for g in gauges
+        ],
+        allowFailureMap=0,
+        startDate=0,
+        endDate=0,
+        voteOption=0,
+        tryEarlyExecution=True
+    ))
+    print(proposal_id)
