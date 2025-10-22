0ff8dcb6ef64f08d81435eb7bf45b415c61413ff
Michael Egorov <michwill@gmail.com>
2025-10-22 16:04:25 +0200

    Check that claiming admin fees works

--- scripts/fork_test/test_pass_factory.py
+++ scripts/fork_test/test_pass_factory.py
@@ -14,6 +14,35 @@ if __name__ == '__main__':
     factory = boa.load_partial('contracts/Factory.vy').at(FACTORY)
     factory_owner = boa.load('contracts/MigrationFactoryOwner.vy', DAO, FACTORY)
 
+    lt_interface = boa.load_partial('contracts/LT.vy')
+    gauge_interface = boa.load_partial('contracts/dao/LiquidityGauge.vy')
+    lts = [lt_interface.at(factory.markets(i).lt) for i in range(3)]
+    gauges = [gauge_interface.at(factory.markets(i).staker) for i in range(3)]
+
+    for i in range(3):
+        pps = lts[i].pricePerShare() / 1e18
+        g_rate = gauges[i].previewRedeem(10**18) / 1e18
+        print("PPS before admin claim:", pps)
+        print("Gauge PPS before admin claim:", pps * g_rate)
+        print()
+
+    # Test admin fees with tBTC
+    with boa.env.prank(DAO):
+        factory.set_fee_receiver(DAO)
+
+    for i in range(3):
+        lts[i].withdraw_admin_fees()
+
+    for i in range(3):
+        print("Admin fees in DAO:", lts[i].balanceOf(DAO) / 1e18)
+
+        pps = lts[i].pricePerShare() / 1e18
+        g_rate = gauges[i].previewRedeem(10**18) / 1e18
+        print("PPS before admin claim:", pps)
+        print("Gauge PPS before admin claim:", pps * g_rate)
+        print("Admin fees left:", lts[i].liquidity().admin / 1e18)
+        print()
+
     print(f"Before: admin = {factory.admin()}, emergency_admin = {factory.emergency_admin()}")
 
     # Pass ownership to the owner
