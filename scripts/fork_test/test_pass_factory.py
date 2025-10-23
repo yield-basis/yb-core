01dab0a3f7efb8157509a4e718f3b3f66e7e006e
Michael Egorov <michwill@gmail.com>
2025-10-23 16:34:53 +0200

    Set new LT implementation and make new markets in the demo script

--- scripts/fork_test/test_pass_factory.py
+++ scripts/fork_test/test_pass_factory.py
@@ -3,8 +3,13 @@ import boa
 from networks import NETWORK
 
 
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 DAO = "0x42F2A41A0D0e65A440813190880c8a65124895Fa"
 FACTORY = "0x370a449FeBb9411c95bf897021377fe0B7D100c0"
+TEST_USER = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"  # <- Just one guy
+
+DUST_AMOUNT = 10**17  # LT shares
+MIGRATE_AMOUNT = 10**17  # LT shares
 
 
 if __name__ == '__main__':
@@ -13,6 +18,7 @@ if __name__ == '__main__':
 
     factory = boa.load_partial('contracts/Factory.vy').at(FACTORY)
     factory_owner = boa.load('contracts/MigrationFactoryOwner.vy', DAO, FACTORY)
+    migrator = boa.load('contracts/LTMigrator.vy', factory.STABLECOIN(), factory_owner.address)
 
     lt_interface = boa.load_partial('contracts/LT.vy')
     gauge_interface = boa.load_partial('contracts/dao/LiquidityGauge.vy')
@@ -45,11 +51,31 @@ if __name__ == '__main__':
 
     print(f"Before: admin = {factory.admin()}, emergency_admin = {factory.emergency_admin()}")
 
+    # Set new LT implementation
+    lt_blueprint = lt_interface.deploy_as_blueprint()
+    with boa.env.prank(DAO):
+        factory.set_implementations(ZERO_ADDRESS, lt_blueprint.address, ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS)
+
+    # Create new markets
+    new_lts = []
+    new_gauges = []
+    with boa.env.prank(DAO):
+        for i in range(3):
+            new_market = factory.add_market(
+                lts[i].CRYPTOPOOL(),
+                int(0.0092 * 1e18),
+                int(0.07 * 1e18 / (86400 * 365)),
+                50 * 10**6 * 10**18)
+            new_lts.append(lt_interface.at(new_market.lt))
+            new_gauges.append(gauge_interface.at(new_market.staker))
+
     # Pass ownership to the owner
     with boa.env.prank(DAO):
         factory.set_admin(factory_owner.address, factory.emergency_admin())
 
     print(f"During migration: admin = {factory.admin()}, emergency_admin = {factory.emergency_admin()}")
+
+    # Use claimed admin fees as a test for deposits and withdrawals
 
     # Pass ownership back
     with boa.env.prank(DAO):
