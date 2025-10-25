d530b3b124837b46a97732aab149f6f156030fd3
Michael Egorov <michwill@gmail.com>
2025-10-25 21:39:20 +0200

    Stage 2: Scripts to create votes for deploying actual markets

---
+++ scripts/voting/create_vote_migration_stage2_2.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import boa
+import os
+import json
+import requests
+
+from eth_account import account
+from collections import namedtuple
+from getpass import getpass
+from networks import NETWORK
+from networks import PINATA_TOKEN
+from time import sleep
+
+
+FORK = True
+MARKET_IDX = 2
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+VOTING_PLUGIN = "0x2be6670DE1cCEC715bDBBa2e3A6C1A05E496ec78"
+DAO = "0x42F2A41A0D0e65A440813190880c8a65124895Fa"
+GAUGE_CONTROLLER = "0x1Be14811A3a06F6aF4fA64310a636e1Df04c1c21"
+FACTORY = "0x370a449FeBb9411c95bf897021377fe0B7D100c0"
+
+USER = "0x337bAbD8cA6911A2de6608e388708F31d07A86Ff"
+
+Proposal = namedtuple("Proposal", ["metadata", "actions", "allowFailureMap", "startDate", "endDate", "voteOption",
+                                   "tryEarlyExecution"])
+Action = namedtuple("Action", ["to", "value", "data"])
+
+
+def pin_to_ipfs(content: dict):
+    url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    headers = {
+        "Authorization": f"Bearer {PINATA_TOKEN}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "pinataContent": content,
+        "pinataMetadata": {"name": "pinnie.json"},
+        "pinataOptions": {"cidVersion": 1},
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    assert 200 <= response.status_code < 400
+
+    return 'ipfs://' + response.json()["IpfsHash"]
+
+
+def account_load(fname):
+    path = os.path.expanduser(os.path.join('~', '.brownie', 'accounts', fname + '.json'))
+    with open(path, 'r') as f:
+        pkey = account.decode_keyfile_json(json.load(f), getpass())
+        return account.Account.from_key(pkey)
+
+
+if __name__ == '__main__':
+    if FORK:
+        boa.fork(NETWORK)
+        boa.env.eoa = USER
+    else:
+        boa.set_network_env(NETWORK)
+        USER = account_load('yb-deployer-c')
+        boa.env.add_account(USER)
+
+    voting = boa.load_abi(os.path.dirname(__file__) + '/TokenVoting.abi.json', name="AragonVoting").at(VOTING_PLUGIN)
+    factory = boa.load_partial('contracts/Factory.vy').at(FACTORY)
+    lt_interface = boa.load_partial('contracts/LT.vy')
+    lt = lt_interface.at(factory.markets(MARKET_IDX).lt)
+    print(lt.symbol())
+
+    proposal_id = voting.createProposal(*Proposal(
+        metadata=pin_to_ipfs({
+            'title': f'Stage 2 of liquidity migration - create {lt.symbol()} market',
+            'summary': '',
+            'resources': []}).encode(),
+        actions=[
+            Action(
+                to=factory.address, value=0,
+                data=factory.add_market.prepare_calldata(
+                    lt.CRYPTOPOOL(),
+                    int(0.0092 * 1e18),
+                    int(0.07 * 1e18 / (86400 * 365)),
+                    50 * 10**6 * 10**18
+                )
+            ),
+        ],
+        allowFailureMap=0,
+        startDate=0,
+        endDate=0,
+        voteOption=0,
+        tryEarlyExecution=True
+    ))
+    print(proposal_id)
