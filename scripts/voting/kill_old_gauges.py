6d5ac9fc3ccb7ddc6da4aacc5e6839f540cefc2d
Michael Egorov <michwill@gmail.com>
2025-11-16 20:54:11 +0100

    Vote to kill old gauges

---
+++ scripts/voting/kill_old_gauges.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import boa
+import os
+import json
+import requests
+
+from eth_account import account
+from collections import namedtuple
+from getpass import getpass
+from networks import NETWORK
+from networks import PINATA_TOKEN
+
+
+FORK = False
+
+VOTING_PLUGIN = "0x2be6670DE1cCEC715bDBBa2e3A6C1A05E496ec78"
+DAO = "0x42F2A41A0D0e65A440813190880c8a65124895Fa"
+GAUGE_CONTROLLER = "0x1Be14811A3a06F6aF4fA64310a636e1Df04c1c21"
+
+USER = "0xa39E4d6bb25A8E55552D6D9ab1f5f8889DDdC80d"
+
+Proposal = namedtuple("Proposal", ["metadata", "actions", "allowFailureMap", "startDate", "endDate", "voteOption",
+                                   "tryEarlyExecution"])
+Action = namedtuple("Action", ["to", "value", "data"])
+
+
+def pin_to_ipfs(content: dict):
+    url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    headers = {
+        "Authorization": f"Bearer {PINATA_TOKEN}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "pinataContent": content,
+        "pinataMetadata": {"name": "pinnie.json"},
+        "pinataOptions": {"cidVersion": 1},
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    assert 200 <= response.status_code < 400
+
+    return 'ipfs://' + response.json()["IpfsHash"]
+
+
+def account_load(fname):
+    path = os.path.expanduser(os.path.join('~', '.brownie', 'accounts', fname + '.json'))
+    with open(path, 'r') as f:
+        pkey = account.decode_keyfile_json(json.load(f), getpass())
+        return account.Account.from_key(pkey)
+
+
+if __name__ == '__main__':
+    if FORK:
+        boa.fork(NETWORK)
+        boa.env.eoa = USER
+    else:
+        boa.set_network_env(NETWORK)
+        USER = account_load('yb-deployer')
+        boa.env.add_account(USER)
+
+    voting = boa.load_abi(os.path.dirname(__file__) + '/TokenVoting.abi.json', name="AragonVoting").at(VOTING_PLUGIN)
+    gc = boa.load_partial('contracts/dao/GaugeController.vy').at(GAUGE_CONTROLLER)
+
+    proposal_id = voting.createProposal(*Proposal(
+        metadata=pin_to_ipfs({
+            'title': 'Kill gauges for obsolete markets',
+            'summary': 'This vote prevents weight-voting for obsolete markets',
+            'resources': []}).encode(),
+        actions=[
+            Action(
+                to=gc.address, value=0,
+                data=gc.set_killed.prepare_calldata(gc.gauges(i), True)
+            )
+            for i in range(3)
+        ],
+        allowFailureMap=0,
+        startDate=0,
+        endDate=0,
+        voteOption=0,
+        tryEarlyExecution=True
+    ))
+    print(proposal_id)
